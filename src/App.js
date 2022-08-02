51b10b108e942cfb004d547cd3565037cf537d3e
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2022-08-02 13:30:00 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBOjfwkfcp8el-7LsFQKetI2zLN0nBWy5A",
+  authDomain: "superchat-app-db722.firebaseapp.com",
+  projectId: "superchat-app-db722",
+  storageBucket: "superchat-app-db722.appspot.com",
+  messagingSenderId: "123998690292",
+  appId: "1:123998690292:web:0606007bc21232a38db7a1",
+  measurementId: "G-GB99MF8R87",
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header>
-
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+        
       </header>
+
       <section>
-        {user ? <ChatRoom/> : <SignIn/>}
+        {user ? <ChatRoom /> : <SignIn />}
       </section>
+
     </div>
   );
 }
 
-export default App;
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+ 
+
+  return (<>
+  
+  </>)
+}
